709dbb5d4ee5556ca3194477eb5a146ef9d8af69
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-14 21:21:54 +0300

    Minor changes

--- asyncawait.js
+++ asyncawait.js
@@ -4,30 +4,43 @@
 //   })
 //   .catch((error) => console.log(error));
 
-function sleep(time) {
-  return new Promise((resolve, reject) => {
-    if (time < 500) {
-      reject("Слишком мало сна");
-    }
-    setTimeout(() => resolve(`Was sleeping for ${time}`), time);
-  });
-}
+const getGitData = async () => {
+  try {
+    const response = await fetch("https://api.github.com/users/Kasaklalita");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-sleep(1500)
-  .then((res) => {
-    console.log(res);
-    return sleep(1000);
-  })
-  .then((res) => {
-    console.log(res);
-    return sleep(500);
-  })
-  .then((res) => {
-    console.log(res);
-    return sleep(1500);
-  })
-  .then((res) => console.log(res))
-  .catch((error) => console.log(error));
+getGitData();
 console.log("asdf");
+
+// function sleep(time) {
+//   return new Promise((resolve, reject) => {
+//     if (time < 500) {
+//       reject("Слишком мало сна");
+//     }
+//     setTimeout(() => resolve(`Was sleeping for ${time}`), time);
+//   });
+// }
+
+// sleep(1500)
+//   .then((res) => {
+//     console.log(res);
+//     return sleep(1000);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return sleep(500);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return sleep(1500);
+//   })
+//   .then((res) => console.log(res))
+//   .catch((error) => console.log(error));
+// console.log("asdf");
 
 // setInterval(() => console.log(Date.now()), 500);
