6338b256a0ac26200d258e3899ae12b00a689578
Veniamin Polienko <kasaklalita@mail.ru>
2023-02-14 18:52:40 +0300

    Sleep promise

--- asyncawait.js
+++ asyncawait.js
@@ -1,5 +1,33 @@
-const myGit = fetch("https://api.github.com/users/Kasaklalita")
-  .then((response) => {
-    return response.json().then((res) => console.log(res));
+// const myGit = fetch("https://api.github.com/users/Kasaklalita")
+//   .then((response) => {
+//     return response.json().then((res) => console.log(res));
+//   })
+//   .catch((error) => console.log(error));
+
+function sleep(time) {
+  return new Promise((resolve, reject) => {
+    if (time < 500) {
+      reject("Слишком мало сна");
+    }
+    setTimeout(() => resolve(`Was sleeping for ${time}`), time);
+  });
+}
+
+sleep(1500)
+  .then((res) => {
+    console.log(res);
+    return sleep(1000);
   })
+  .then((res) => {
+    console.log(res);
+    return sleep(500);
+  })
+  .then((res) => {
+    console.log(res);
+    return sleep(1500);
+  })
+  .then((res) => console.log(res))
   .catch((error) => console.log(error));
+console.log("asdf");
+
+// setInterval(() => console.log(Date.now()), 500);
